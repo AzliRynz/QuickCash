d8e7fb25bb6d11ebc743d1f64559e6a8d863ee69
AzliRynz <135945845+AzliRynz@users.noreply.github.com>
2025-03-27 19:39:58 +0700

    Update Main.php

--- src/NurAzliYT/QuickCash/Main.php
+++ src/NurAzliYT/QuickCash/Main.php
@@ -3,11 +3,6 @@
 namespace NurAzliYT\QuickCash;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\plugin\PluginDescription;
-use pocketmine\plugin\PluginLoader;
-use pocketmine\plugin\PluginLogger;
-use pocketmine\plugin\ResourceProvider;
-use pocketmine\scheduler\TaskScheduler;
 use pocketmine\Server;
 use pocketmine\utils\Config;
 use NurAzliYT\QuickCash\api\QuickCashAPI;
@@ -16,44 +11,20 @@ use NurAzliYT\QuickCash\commands\{SetCashCommand, SeeCashCommand, AddCashCommand
 class Main extends PluginBase{
 
     private Config $config;
-    private PluginLogger $logger;
-	private TaskScheduler $scheduler;
-
-    public function __construct(
-		private PluginLoader $loader,
-		private Server $server,
-		private PluginDescription $description,
-		private string $dataFolder,
-		private string $file,
-		private ResourceProvider $resourceProvider
-	){
-		$this->dataFolder = rtrim($dataFolder, "/" . DIRECTORY_SEPARATOR) . "/";
-		//TODO: this is accessed externally via reflection, not unused
-		$this->file = rtrim($file, "/" . DIRECTORY_SEPARATOR) . "/";
-		$this->resourceFolder = Path::join($this->file, "resources") . "/";
-
-		$this->configFile = Path::join($this->dataFolder, "config.yml");
-
-		$prefix = $this->description->getPrefix();
-		$this->logger = new PluginLogger($server->getLogger(), $prefix !== "" ? $prefix : $this->getName());
-		$this->scheduler = new TaskScheduler($this->getFullName());
-
-		$this->onLoad();
-
-		$this->registerYamlCommands();
-    }
 
     public function onEnable() : void{
         $this->saveDefaultConfig();
-        $this->config = $this->getConfig();
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
 
-        $this->getServer()->getCommandMap()->registerAll(null, [
+        $this->getServer()->getCommandMap()->registerAll($this->getName(), [
             new SetCashCommand($this),
             new SeeCashCommand($this),
             new AddCashCommand($this),
             new RemoveCashCommand($this),
             new PayCommand($this)
         ]);
+
+        $this->getLogger()->info("QuickCash telah diaktifkan!");
     }
 
     public function getConfigValue(string $key, $default = null){
@@ -65,11 +36,11 @@ class Main extends PluginBase{
     }
 
     public function getDefaultBalance() : int{
-        return $this->getConfigValue("default-balance", 1000);
+        return (int) $this->getConfigValue("default-balance", 1000);
     }
 
     public function getMaxBalance() : int{
-        return $this->getConfigValue("max-balance", 1000000);
+        return (int) $this->getConfigValue("max-balance", 1000000);
     }
 
     public function getAPI() : QuickCashAPI{
